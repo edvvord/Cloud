f8a349b72a444b23aed70dfad8a29f7fc086ec68
_Edword_ <94526552+edvvord@users.noreply.github.com>
2026-02-08 16:19:10 +0500

    Enhance upload function to support cape file

--- upload.js
+++ upload.js
@@ -1,21 +1,22 @@
 async function upload() {
-    const file = document.getElementById("skin").files[0];
-    if (!file) return alert("Select PNG skin");
-
+    const skinFile = document.getElementById("skin").files[0];
+    const capeFile = document.getElementById("cape").files[0];
+    
+    if (!skinFile && !capeFile) return alert("Select PNG skin or cape");
+    
     const form = new FormData();
-    form.append("skin", file);
-
+    if (skinFile) form.append("skin", skinFile);
+    if (capeFile) form.append("cape", capeFile);
+    
     const res = await fetch("/api/upload", {
         method: "POST",
         body: form
     });
-
     if (!res.ok) {
-        const text = await res.text(); // для дебага
+        const text = await res.text();
         console.error(text);
         return alert("Upload failed");
     }
-
     const json = await res.json();
     document.getElementById("out").textContent =
         JSON.stringify(json, null, 2);
